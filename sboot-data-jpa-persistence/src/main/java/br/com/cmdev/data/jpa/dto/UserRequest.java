4370da956c9ff1311edcab13315b75a84ab5e6b3
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2025-04-16 19:21:32 -0300

    Remoção de imports sem uso

--- sboot-data-jpa-persistence/src/main/java/br/com/cmdev/data/jpa/dto/UserRequest.java
+++ sboot-data-jpa-persistence/src/main/java/br/com/cmdev/data/jpa/dto/UserRequest.java
@@ -1,6 +1,5 @@
 package br.com.cmdev.data.jpa.dto;
 
-import br.com.cmdev.data.jpa.entity.Address;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
